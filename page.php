f25be1a085803a1c79ff3cf69acd6bf8fc505bb0
Michael Pirali <pirali.michael@gmail.com>
2019-10-10 14:35:59 +0200

    update

--- page.php
+++ page.php
@@ -1,8 +1,19 @@
-<?php 
+<?php
 /*
 Template Name: PAGE
 */
 get_header();
 echo ('template page');
 get_footer();
+?>
+<?php
+// TO SHOW THE PAGE CONTENTS
+    while ( have_posts() ) : the_post(); ?> <!--Because the_content() works only inside a WP Loop -->
+        <div class="entry-content-page">
+            <?php the_content(); ?> <!-- Page Content -->
+        </div><!-- .entry-content-page -->
+
+    <?php
+endwhile; //resetting the page loop
+wp_reset_query(); //resetting the page query
 ?>
