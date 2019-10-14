571d16fa9734d2cee0841263207c5bc620bdc321
Jfs <jf.stassen@gmail.com>
2019-10-14 20:50:03 +0200

    start bandeau version grid

--- front-page.php
+++ front-page.php
@@ -8,7 +8,9 @@ get_header();
 <div class="bandeau">
 	<div class="bandeau-img-left">
         <img src="<?php echo get_field('bandeau_image_g')['sizes']['home_top']; ?>">
-        <div class="container-content-g">
+        
+    </div>
+    <div class="container-content-g">
             <a href="#">
 		    <div class="bandeau-titre-g">
                 <h1 class="title"><?php the_field('bandeau_titre_g') ?></h1>
@@ -18,7 +20,6 @@ get_header();
             </div>
             </a>
         </div>
-	</div>
 	<div class="bandeau-img-right">
 		<img src="<?php echo get_field('bandeau_image_d')['sizes']['home_top']; ?>">
 		<h1 class="title"><?php the_field('bandeau_titre_d') ?></h1>
