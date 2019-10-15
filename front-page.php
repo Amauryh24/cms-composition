e5c70c41654d0980afcc499d44325cae27b34fe2
Jfs <jf.stassen@gmail.com>
2019-10-15 12:40:18 +0200

    skip bandeau

--- front-page.php
+++ front-page.php
@@ -8,18 +8,19 @@ get_header();
 <div class="bandeau">
 	<div class="bandeau-img-left">
         <img src="<?php echo get_field('bandeau_image_g')['sizes']['home_top']; ?>">
-        
-    </div>
-    <div class="container-content-g">
-            <a href="#">
-		    <div class="bandeau-titre-g">
-                <h1 class="title"><?php the_field('bandeau_titre_g') ?></h1>
-            </div>
-            <div class="bandeau-texte-g">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, sapiente. Provident, maxime ea fugiat at temporibus iste ut magni unde officia soluta odio sequi sed, est possimus! Voluptatibus, corrupti architecto, impedit id repellendus doloribus quod natus beatae unde voluptas explicabo.</p>
-            </div>
-            </a>
+
+        <a href="#">
+        <div class="container-content-g">    
+                <div class="bandeau-titre-g">
+                    <h1 class="title"><?php the_field('bandeau_titre_g') ?></h1>
+                </div>
+                <div class="bandeau-texte-g">
+                    <p>Voyagez dans un espace découpé en zones thématiques, entre des marques mondialement renommées, des designers exclusifs et des créateurs belges.</p>
+                </div>
         </div>
+        </a>
+    </div>
+    
 	<div class="bandeau-img-right">
 		<img src="<?php echo get_field('bandeau_image_d')['sizes']['home_top']; ?>">
 		<h1 class="title"><?php the_field('bandeau_titre_d') ?></h1>
