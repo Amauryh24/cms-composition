d47e21474a28fcabdd1b17460a352d7118857689
Jfs <jf.stassen@gmail.com>
2019-10-20 16:56:32 +0200

    ebauche menu responsive v1.5

--- js/main.js
+++ js/main.js
@@ -1,16 +1,22 @@
-const navslide = () => {
+const navSlide = () => {
     const burger = document.querySelector(".burger");
     const nav = document.querySelector(".res-menu");
     const navLinks = document.querySelectorAll(".res-menu li");
-
+    //click sur le burger pour faire apparaitre le menu
     burger.addEventListener("click", () => {
         nav.classList.toggle("nav-active");
         navLinks.forEach((link, index) =>{
             link.style.animation ? link.style.animation = "" : link.style.animation = `fading 0.5s ease forwards ${index / 7 + 0.5}s`
         });
-
         burger.classList.toggle("toggle");
     });
-    
+    //click sur n'importe ou en zone noire dans le menu deployé pour le faire disparaitre
+    nav.addEventListener("click", () => {
+        nav.classList.toggle("nav-active");
+        navLinks.forEach((link, index) =>{
+            link.style.animation ? link.style.animation = "" : link.style.animation = `fading 0.5s ease forwards ${index / 7 + 0.5}s`
+        });
+        burger.classList.toggle("toggle");
+    });
 };
-navslide();
+navSlide();
