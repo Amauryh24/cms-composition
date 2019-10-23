4396fc66c4204d2d67f487beeb67d4424ca0b775
Michael Pirali <pirali.michael@gmail.com>
2019-10-23 16:33:27 +0200

    update OpenStreetMap

--- page-contact.php
+++ page-contact.php
@@ -6,7 +6,7 @@ get_header();
 ?>
 <h1><?php the_title(); ?></h1>
 
-<iframe width="660px" height="350px" frameborder="0" allowfullscreen src="https://umap.openstreetmap.fr/fr/map/meubles-composition_379823#12/50.5104  /5.2763?scaleControl=false&miniMap=false&scrollWheelZoom=false&zoomControl=false&allowEdit=false&moreControl=false&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=false&onLoadPanel=undefined&captionBar=false"></iframe>
+<iframe width="660px" height="350px" frameborder="0" allowfullscreen src="https://umap.openstreetmap.fr/fr/map/meubles-composition_379823#12/50.5104/5.2763?scaleControl=false&miniMap=false&scrollWheelZoom=false&zoomControl=false&allowEdit=false&moreControl=false&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=false&onLoadPanel=undefined&captionBar=false"></iframe>
 
 <?php
 // https://umap.openstreetmap.fr/fr/map/meubles-composition_379823
