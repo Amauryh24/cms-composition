629b0f16ac28d7d2c53ca82be387f1eefcc5e0d5
Jenhtc <htc.jennifer@gmail.com>
2019-10-24 09:13:48 +0200

    COMMIT

--- header.php
+++ header.php
@@ -1,4 +1,9 @@
-<?php 
-
-echo ('header');
-?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo('charset'); ?>">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<?php wp_head(); ?>
+<title>Composition</title>
+</head>
--- page-contact.php
+++ page-contact.php
@@ -3,7 +3,17 @@
 Template Name: PAGE-contact
 */
 get_header();
-echo ('coucou contact');
-get_footer();
 ?>
 <h1><?php the_title(); ?></h1>
+<?php
+$form = new WP_Query(array('page_id' => 29));
+if($form->have_posts()) :
+    while($form->have_posts()) : $form->the_post();
+    the_content();
+endwhile;
+else:
+    echo('quedal');
+endif;
+get_footer();
+?>
+
