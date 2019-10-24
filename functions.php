5600f116ab3b388b658ce400603981201a205547
Michael Pirali <pirali.michael@gmail.com>
2019-10-24 14:18:08 +0200

    update functions.php

--- functions.php
+++ functions.php
@@ -49,4 +49,29 @@ function project_setup() {
 add_action('after_setup_theme', 'project_setup');
 
 register_nav_menu( 'primary', 'Primary Menu' );
+
+if( function_exists('acf_add_options_page') ) {
+	// Page principale
+	acf_add_options_page(array(
+		'page_title'    => 'Options',
+		'menu_title'    => 'Options',
+		'menu_slug'     => 'options-generales',
+		'capability'    => 'edit_posts',
+		'redirect'      => true
+	));
+
+  // Première sous-page
+	acf_add_options_sub_page(array(
+		'page_title'    => 'Options d\'Entête',
+		'menu_title'    => 'Entête',
+		'parent_slug'   => 'options-generales',
+	));
+  // Deuxième sous-page
+	acf_add_options_sub_page(array(
+		'page_title'    => 'Options de Pied de Page',
+		'menu_title'    => 'Pied de page',
+		'parent_slug'   => 'options-generales',
+	));
+}
+
 ?>
