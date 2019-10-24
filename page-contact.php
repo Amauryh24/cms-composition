8a777f652c8fab84c6c43ef3c1f8ccdd7e6bbcef
amaury2409 <amaury.henrottay@gmail.com>
2019-10-24 15:36:13 +0200

    fix logo

--- page-contact.php
+++ page-contact.php
@@ -6,9 +6,14 @@ get_header();
 ?>
 <h1><?php the_title(); ?>
 </h1>
+<div class="row">
+    <div class="col s12 m12 l6 offset-l4 ">
+        <iframe width="100%" height="350px" frameborder="0" allowfullscreen
+            src="<?php the_field('openstreetmap_lien'); ?>#<?php the_field('openstreetmap_zoom'); ?>/<?php the_field('openstreetmap_latitude'); ?>/<?php the_field('openstreetmap_longitude'); ?>?scaleControl=false&miniMap=false&scrollWheelZoom=false&zoomControl=false&allowEdit=false&moreControl=false&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=false&onLoadPanel=undefined&captionBar=false"></iframe>
 
-<iframe width="660px" height="350px" frameborder="0" allowfullscreen
-    src="<?php the_field('openstreetmap_lien'); ?>#<?php the_field('openstreetmap_zoom'); ?>/<?php the_field('openstreetmap_latitude'); ?>/<?php the_field('openstreetmap_longitude'); ?>?scaleControl=false&miniMap=false&scrollWheelZoom=false&zoomControl=false&allowEdit=false&moreControl=false&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=false&onLoadPanel=undefined&captionBar=false"></iframe>
+    </div>
+</div>
+
 <div class="row">
     <div class="col s10 m10 l4 offset-s1 offset-m1 offset-l1">
         <h3>Une questions, une demande d'offre ?</h3>
