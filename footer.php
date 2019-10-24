594a8c9fc7eeaeb2deb0973c1ae303cb9d138e5d
amaury2409 <amaury.henrottay@gmail.com>
2019-10-24 14:51:52 +0200

    ajout contact

--- footer.php
+++ footer.php
@@ -62,93 +62,117 @@ foreach ( $numbers as $n => $c )
 
 
 <footer id="mic">
-  <div class="footer-content">
-    <div class="row section">
+    <div class="footer-content">
+        <div class="row section">
 
-      <!-- SPECIAL SECTION tissus -->
-      <div class="col s12 m8">
-        <!-- ROUND ICON tssus -->
-        <div class="col s12 m3 center-align">
-          <img src="http://localhost:8080/cms-composition/wp-content/themes/cms-composition/images/footer/footer_250x250_01.jpg" width="150px" alt="Image ronde d'un tissu vu de près" class="circle responsive-img"> <!-- notice the "circle" class -->
-        </div>
-        <div class="col s12 m9 center-align">
-          <!-- PAGE LINK tissus -->
-          <p>
-            <a href="#" class="valign-wrapper large">Tissus et habillage maison <img src = '<?php bloginfo('template_directory'); ?>/svg/fleche-droite.svg' alt = '' class="flechette" />
-            </a>
-          </p>
-        </div>
-      </div> <!-- end of SPECIAL SECTION tissus -->
+            <!-- SPECIAL SECTION tissus -->
+            <div class="col s12 m8">
+                <!-- ROUND ICON tssus -->
+                <div class="col s12 m3 center-align">
+                    <img src="<?php bloginfo('template_directory'); ?>/images/footer/footer_250x250_01.jpg"
+                        width="150px" alt="Image ronde d'un tissu vu de près" class="circle responsive-img">
+                    <!-- notice the "circle" class -->
+                </div>
+                <div class="col s12 m9 center-align">
+                    <!-- PAGE LINK tissus -->
+                    <p>
+                        <a href="#" class="valign-wrapper large">Tissus et habillage maison <img
+                                src='<?php bloginfo('template_directory'); ?>/svg/fleche-droite.svg' alt=''
+                                class="flechette" />
+                        </a>
+                    </p>
+                </div>
+            </div> <!-- end of SPECIAL SECTION tissus -->
 
-      <!-- OPEN HOURS black chart -->
-      <div class="col s12 m4 black white-text chart">
-        <div class="col s1">
-          <h5><img src = '<?php bloginfo('template_directory'); ?>/svg/time-clock.svg' height="17px" alt = '' /></h5>
-        </div>
-        <div class="col s11">
-          <h5>Horaires</h5>
-          <p>
-            Lundi > Dimanche : (Fermé le mardi)
-          </p>
-          <p>
-            <?php the_field('intervalle_ouverture', 'option'); ?> > <?php the_field('intervalle_fermeture', 'option'); ?>
-          </p>
-        </div>
-      </div>
-    </div> <!-- end of FOOTER TOP -->
+            <!-- OPEN HOURS black chart -->
+            <div class="col s12 m4 black white-text chart">
+                <div class="col s1">
+                    <h5><img src='<?php bloginfo('template_directory'); ?>/svg/time-clock.svg' height="17px" alt='' />
+                    </h5>
+                </div>
+                <div class="col s11">
+                    <h5>Horaires</h5>
+                    <p>
+                        Lundi > Dimanche : (Fermé le mardi)
+                    </p>
+                    <p>
+                        <?php the_field('intervalle_ouverture', 'option'); ?> >
+                        <?php the_field('intervalle_fermeture', 'option'); ?>
+                    </p>
+                </div>
+            </div>
+        </div> <!-- end of FOOTER TOP -->
 
-    <!-- GREY LINE between top and bottom footer -->
-    <div class="divider"></div>
+        <!-- GREY LINE between top and bottom footer -->
+        <div class="divider"></div>
 
-    <!-- FOOTER BOTTOM -->
-    <div class="row section">
-      <div class="col s12 m4">
-        <h4>Rendez-nous visite !</h4>
-        <!-- Google map link -->
-        <p>
-          <a href="<?php the_field('lien', 'option'); ?>" target="_blank" class="valign-wrapper large">Google map <img src = '<?php bloginfo('template_directory'); ?>/svg/fleche-droite.svg' alt = '' class="flechette" />
-          </a>
-        </p>
-      </div>
+        <!-- FOOTER BOTTOM -->
+        <div class="row section">
+            <div class="col s12 m4">
+                <h4>Rendez-nous visite !</h4>
+                <!-- Google map link -->
+                <p>
+                    <a href="<?php the_field('lien', 'option'); ?>" target="_blank" class="valign-wrapper large">Google
+                        map <img src='<?php bloginfo('template_directory'); ?>/svg/fleche-droite.svg' alt=''
+                            class="flechette" />
+                    </a>
+                </p>
+            </div>
 
-      <!-- CONTACT INFOS -->
-      <div class="col s12 m4">
-        <!-- STREET ADDRESSE -->
-        <p class="valign-wrapper">
-          <img src = '<?php bloginfo('template_directory'); ?>/svg/marker-road.svg' alt = '' /><?php the_field('adresse_prem_ligne_rue', 'option'); ?>,  <?php the_field('adresse_prem_ligne_numero', 'option'); ?> - <?php the_field('adresse_sec_ligne_code_postal', 'option'); ?> <?php the_field('adresse_sec_ligne_localite', 'option'); ?>
-        </p>
-        <!-- EMAIL and action link -->
-        <p>
-          <a href="mailto:<?php the_field('email', 'option'); ?>" class="valign-wrapper"><img src = '<?php bloginfo('template_directory'); ?>/svg/icon-arrobas.svg' alt = '' /><?php the_field('email', 'option'); ?>
-          </a>
-        </p>
-        <!-- PHONE NUMBER and action link -->
-        <p>
-          <a href="tel:<?php echo $n; ?>" class="valign-wrapper"><img src = '<?php bloginfo('template_directory'); ?>/svg/phone-2.svg' alt = '' /><?php echo $phone; ?>
-          </a>
-        </p>
-      </div> <!-- end of CONTACT INFOS -->
+            <!-- CONTACT INFOS -->
+            <div class="col s12 m4">
+                <!-- STREET ADDRESSE -->
+                <p class="valign-wrapper">
+                    <img src='<?php bloginfo('template_directory'); ?>/svg/marker-road.svg'
+                        alt='' /><?php the_field('adresse_prem_ligne_rue', 'option'); ?>,
+                    <?php the_field('adresse_prem_ligne_numero', 'option'); ?> -
+                    <?php the_field('adresse_sec_ligne_code_postal', 'option'); ?>
+                    <?php the_field('adresse_sec_ligne_localite', 'option'); ?>
+                </p>
+                <!-- EMAIL and action link -->
+                <p>
+                    <a href="mailto:<?php the_field('email', 'option'); ?>" class="valign-wrapper"><img
+                            src='<?php bloginfo('template_directory'); ?>/svg/icon-arrobas.svg'
+                            alt='' /><?php the_field('email', 'option'); ?>
+                    </a>
+                </p>
+                <!-- PHONE NUMBER and action link -->
+                <p>
+                    <a href="tel:<?php echo $n; ?>" class="valign-wrapper"><img
+                            src='<?php bloginfo('template_directory'); ?>/svg/phone-2.svg'
+                            alt='' /><?php echo $phone; ?>
+                    </a>
+                </p>
+            </div> <!-- end of CONTACT INFOS -->
 
-      <!-- SOCIAL NETWORK SVG ICONS -->
-      <div class="col s12 m4 right-align">
-        <p>
-          <!-- FACEBOOK SVG ICON -->
-          <a class="btn-floating btn-large waves-effect waves-black white"><img src = '<?php bloginfo('template_directory'); ?>/svg/logo-facebook.svg' alt = '' height="24px" class="facebook" />
-          </a>
-          <!-- INSTAGRAM SVG ICON -->
-          <a class="btn-floating btn-large waves-effect waves-black white"><img src = '<?php bloginfo('template_directory'); ?>/svg/logo-instagram.svg' alt = '' height="24px" class="instapin" />
-          </a>
-          <!-- PINTEREST SVG ICON -->
-          <a class="btn-floating btn-large waves-effect waves-black white"><img src = '<?php bloginfo('template_directory'); ?>/svg/logo-pinterest.svg' alt = '' height="24px" class="instapin" />
-          </a>
-        </p>
-        <!-- COPYRIGHT and SGPDR -->
-        <p>
-          <a href="#" class="mini">© Composition 2019</a> - <a href="#" class="mini">politique de confidentialité</a>
-        </p>
-      </div> <!-- end of SOCIAL NETWORK -->
-    </div> <!-- end of FOOTER BOTTOM -->
-  </div>
+            <!-- SOCIAL NETWORK SVG ICONS -->
+            <div class="col s12 m4 right-align">
+                <p>
+                    <!-- FACEBOOK SVG ICON -->
+                    <a class="btn-floating btn-large waves-effect waves-black white"><img
+                            src='<?php bloginfo('template_directory'); ?>/svg/logo-facebook.svg' alt='' height="24px"
+                            class="facebook" />
+                    </a>
+                    <!-- INSTAGRAM SVG ICON -->
+                    <a class="btn-floating btn-large waves-effect waves-black white"><img
+                            src='<?php bloginfo('template_directory'); ?>/svg/logo-instagram.svg' alt='' height="24px"
+                            class="instapin" />
+                    </a>
+                    <!-- PINTEREST SVG ICON -->
+                    <a class="btn-floating btn-large waves-effect waves-black white"><img
+                            src='<?php bloginfo('template_directory'); ?>/svg/logo-pinterest.svg' alt='' height="24px"
+                            class="instapin" />
+                    </a>
+                </p>
+                <!-- COPYRIGHT and SGPDR -->
+                <p>
+                    <a href="#" class="mini">© Composition 2019</a> - <a href="#" class="mini">politique de
+                        confidentialité</a>
+                </p>
+            </div> <!-- end of SOCIAL NETWORK -->
+        </div> <!-- end of FOOTER BOTTOM -->
+    </div>
 </footer>
 </body>
+
 </html>
--- page-contact.php
+++ page-contact.php
@@ -4,16 +4,31 @@ Template Name: PAGE-contact
 */
 get_header();
 ?>
-<h1><?php the_title(); ?></h1>
-<?php
-$form = new WP_Query(array('page_id' => 29));
+<h1><?php the_title(); ?>
+</h1>
+
+<iframe width="660px" height="350px" frameborder="0" allowfullscreen
+    src="<?php the_field('openstreetmap_lien'); ?>#<?php the_field('openstreetmap_zoom'); ?>/<?php the_field('openstreetmap_latitude'); ?>/<?php the_field('openstreetmap_longitude'); ?>?scaleControl=false&miniMap=false&scrollWheelZoom=false&zoomControl=false&allowEdit=false&moreControl=false&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=false&onLoadPanel=undefined&captionBar=false"></iframe>
+<div class="row">
+    <div class="col s10 m10 l4 offset-s1 offset-m1 offset-l1">
+        <h3>Une questions, une demande d'offre ?</h3>
+        <p>Formulaire de contact</p>
+    </div>
+    <div class="col s10 m10 l4 offset-s1 offset-m1 offset-l1">
+        <?php
+$form = new WP_Query(array('page_id' => 44));
 if($form->have_posts()) :
     while($form->have_posts()) : $form->the_post();
     the_content();
 endwhile;
 else:
     echo('quedal');
-endif;
+endif;?>
+
+    </div>
+
+</div>
+
+<?php
 get_footer();
 ?>
-
